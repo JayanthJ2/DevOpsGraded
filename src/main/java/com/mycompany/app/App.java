7e875dcfa80b9db33b0d966c9a84b44ceb364423
JayanthJ2 <95409863+JayanthJ2@users.noreply.github.com>
2021-12-09 21:51:53 +0530

    comitted the changes

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -1,13 +1,13 @@
 package com.mycompany.app;
 
 /**
- * Hello Dev-ops!
+ * Hello Dev-Ops!
  *
  */
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello Dev-ops" );
+        System.out.println( "Hello Dev-Ops" );
     }
 }
